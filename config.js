7ceed878e9b196aed10f6296ccb67f894291e809
genecarles <genecarles@gmail.com>
2026-05-10 18:11:28 +0200

    AAA

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
 
     // 1. NEGOCI
-    NOM:            "AGORA",
+    NOM:            "ÀGORA",
     LOGO:           "Icon/logoAVtrans.png",
     SLOGAN:         "El teu eslògan aquí",
     TELEFON:        "+34 000 000 000",
