78d00df5af730a0bb55486e7c990e96dcafb75aa
genecarles <genecarles@gmail.com>
2026-05-17 13:18:41 +0200

    dghb

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     TELEFON:        "+34 000 000 000",
     EMAIL:          "agora@altervector.com",
     ADRECA:         "Carrer, número, població",
-    INSTAGRAM:      "https://www.instagram.com/agoraplazavella"
+    INSTAGRAM:      "https://www.instagram.com/agoraplazavella",
     FACEBOOK:       "https://www.facebook.com/",
 
     // 2. RUTES (en local, tot és relatiu)
