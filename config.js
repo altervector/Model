dda4d2060adfc64de66b5fd8b0aa9c688ae9086e
genecarles <genecarles@gmail.com>
2026-05-20 15:50:30 +0200

    fgh

--- config.js
+++ config.js
@@ -8,23 +8,28 @@ const CONFIG = {
     // 1. NEGOCI
     NOM:            "àgora",
     LOGO:           "Icon/logoAGtrans.png",
-    SLOGAN:         "El teu eslògan aquí",
+    SLOGAN:         "Plaça Vella",
     TELEFON:        "+34 000 000 000",
     EMAIL:          "agora@altervector.com",
     ADRECA:         "Carrer, número, població",
     INSTAGRAM:      "https://www.instagram.com/agoraplazavella",
     FACEBOOK:       "https://www.facebook.com/",
+    EMAIL_SUPORT:   "suport@altervector.com",
+
 
     // 2. RUTES (en local, tot és relatiu)
+    REPO_URL:       "https://altervector.github.io/agora/",
     BASE_URL:       "./",
-    BASE_WORKER: "https://agora.altervector.workers.dev",
+    BASE_WORKER:    "https://agora.altervector.workers.dev",
+    URL_OFICIAL:    "https://agora.altervector.com",
 
-    // 3. COLORS (per si cal canviar-los des de JS)
+
+    // 3. SEGURETAT (de moment buit, s'activa quan pujem a producció)
+    SITIOS_SEGUROS:  ["altervector.com", "pages.dev", "altervector.github.io", "localhost", "127.0.0.1"],
+    URL_OFICIAL:    "",
+    
+    
+    // 4. COLORS (per si cal canviar-los des de JS)
     COLOR_PRINCIPAL: "#2c3e35",
     COLOR_ACCENT:    "#c8973a",
-
-    // 4. SEGURETAT (de moment buit, s'activa quan pujem a producció)
-    SITIOS_SEGUROS: ["localhost", "127.0.0.1", "altervector.github.io","agora.altervector.com", "nouprojecte.pages.dev"],
-    URL_OFICIAL:    "",
-
 };
